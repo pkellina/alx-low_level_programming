0bfc8413e9ff0960b07dd8cb9ede1bfd8f1d743b
pkellina <pkellina1@gmail.com>
2022-09-08 18:55:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/**
- *  * printing a phrase on the screen
- *   * Return: 0 if exited properly, non-zero otherwise
- *    */
+/*
+ *printing a phrase on the screen
+ *Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
