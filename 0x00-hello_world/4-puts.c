0c3a4bc95d8f9ddb5f7cdfddb4f89a221e74dcd6
pkellina <pkellina1@gmail.com>
2022-09-08 19:08:53 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main- entry point
- *   *
- *    * Return: ALways returns 0 for smooth execution
- *     */
+ * main- entry point
+ *
+ *Return: ALways returns 0 for smooth execution
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
