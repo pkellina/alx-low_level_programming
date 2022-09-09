2b537fd56d8bf2b639e580894f62ed35140bb547
pkellina <pkellina1@gmail.com>
2022-09-08 18:50:12 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *  * printing a phrase on the screen
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
