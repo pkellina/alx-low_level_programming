bb71d69760a0c2f75402c4263c14a54adc1d59dc
pkellina <pkellina1@gmail.com>
2022-09-09 22:29:01 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int d = 48;
+	while (d<58)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
+
