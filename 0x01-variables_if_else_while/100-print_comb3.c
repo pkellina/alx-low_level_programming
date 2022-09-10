b684c539c357c53614e33aa4b53fa3a279009e33
pkellina <pkellina1@gmail.com>
2022-09-09 23:59:46 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: returns 0 for success
+ */
+int main(void)
+{
+	int d = 48, a = 48;
+	
+	while (d < 58)
+	{
+		a = 58;
+		while (a < 58)
+		{
+			if (d != a && d < a)
+			{
+				putchar(d);
+				putchar(a);
+				if (!(d == 56 && d < a)
+				{
+					putchar(',')
+					putchar(' ')
+				}
+			}
+			a++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return(0);
+}
