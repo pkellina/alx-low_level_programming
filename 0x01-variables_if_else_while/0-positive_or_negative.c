e47ef9c9fe51426ee6f3a873e2f0ad32b1eb5338
pkellina <pkellina1@gmail.com>
2022-09-09 19:14:04 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0 )
 		printf("%i is positive\n" , n);
-	else if (i == 0)
+	else if (n == 0)
 	 	printf("%i is zero\n" , n);
-	else (n < 0)
+	else
 		printf("%i is negative\n" , n);
 	return (0);
 }
