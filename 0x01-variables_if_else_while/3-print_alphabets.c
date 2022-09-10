92730d27c2eb0ebe0b654098d7259c1c7604d0e9
pkellina <pkellina1@gmail.com>
2022-09-09 21:53:39 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  * main - prints lowercase alphabets
+ *   * followed by newline
+ *    * Return: returns 0 for success
+ *     */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
