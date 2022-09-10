974491b1e9955ac91b8490a6f26fb25a9a24bf89
pkellina <pkellina1@gmail.com>
2022-09-09 23:28:58 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * Return: Alwyas 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int d = 48;
+	
 	while (d < 58)
 	{
 		putchar(d);
 		if (d! = 57)
 		{
-			putchar(d);
-			if (d! = 57)
-			{
-				putchar(',');
-				putchar('');
-			}
-			d++;
+			putchar(',');
+			putchar('');
+		}
+		d++;
 		}
 		putchar('\n');
 	return(0);
