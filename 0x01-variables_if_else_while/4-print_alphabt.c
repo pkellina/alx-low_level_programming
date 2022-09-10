0a5777deebbbea147d7d80e2f7afeae5781e36aa
pkellina <pkellina1@gmail.com>
2022-09-09 22:09:19 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabets
+ * followed by newline, except q and e
+ * Return: returns 0 for success
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);		
+		ch++;
+	}
+	putchar('\n');		
+	return (0);
+}
+
