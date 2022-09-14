8c3243f508bddb587faa5e9136f38c0b0507dbc5
pkellina <pkellina1@gmail.com>
2022-09-13 20:36:30 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print lower case alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+}		
