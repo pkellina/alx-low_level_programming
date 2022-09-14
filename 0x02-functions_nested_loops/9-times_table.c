9168305c2bb09587ef69918a897cae1222aed90b
pkellina <pkellina1@gmail.com>
2022-09-14 13:55:51 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * time_tale - prints the 9 times table
+ * times_table - prints the 9 times table
  *
- * main: prints the 9 times table
+ * Description: prints the 9 times table
  * Return: void
  */
 
