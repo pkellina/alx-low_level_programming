910ab393e5d9aa9a146707a6191d3ccf6c49a5f4
pkellina <pkellina1@gmail.com>
2022-09-13 21:00:03 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * 
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int iteration = 0;
+	char letter = 'a';
+
+	while (iteration < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{	
+			_putchar(letter);										 letter++;										  }												   _putchar('\n');										    iteration++;
+	}
+}
