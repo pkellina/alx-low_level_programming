8cef24a2389957bec16d29077f5af8fbb585d254
pkellina <pkellina1@gmail.com>
2022-09-13 22:51:24 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,48 +1,18 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * print_last_digit - prints the last digit of a number
+ * @n: n is an integer
  * 
- * Description: prints every minute of the day
+ * Description: prints the last digit of a number
  * 
- * Return: void
+ * Return: integer
  */
-
-void jack_bauer(void)
+int print_last_digit(int n)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-	hours_max = 58;
-	hours_tens = '0';
-
-	while (hours_tens < '3')
-	{
-		if (hours_tens == '2')
-		{
-			hours_max = '4';
-		}
-		hours_ones = '0';
-		while (hours_ones < hours_max)
-		{
-			minutes_tens = '0';
-			while (minutes_tens < '6')
-			{
-				minutes_ones = '0';
-				while (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar('\n');
-					minutes_ones++;
-				}
-				minutes_ones = '0';
-				minutes_tens++;
-			}
-				minutes_tens = '0';
-				hours_ones++;
-		}
-				hours_ones = '0';
-				hours_tens++;
-	}
+	int last = n % 10;
+	if (n < 0)
+		last = last * -1;
+	_putchar(last + '0');
+	return (last);
 }
