bad8ef6929fd5d81f1e0385d91f7801d5203de84
pkellina <pkellina1@gmail.com>
2022-09-13 21:47:57 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: n is an integer
+ * 
+ * Return: 1 for positve, 0 for zero and -1 for negetive numbers
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	}
+		_putchar('0');
+		return (0);
+	}
+}
