5c9867529a77719199a86c14212e44516ca7adcb
pkellina <pkellina1@gmail.com>
2022-09-14 16:09:45 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,14 +16,14 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 
-				res = (i *j);
-				if (j !=0)
+				res = (i * j);
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 
 				}
-				if (res < 10 && j !=0)
+				if (res < 10 && j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
@@ -36,7 +36,7 @@ void print_times_table(int n)
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
 				}
-				else if (res >= 100 && j !=0)
+				else if (res >= 100 && j != 0)
 				{
 					_putchar((res / 100) + '0');
 					 _putchar((res / 10) % 10 + '0');
