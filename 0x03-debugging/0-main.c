60483352a795e061ce8c91d82af4963e73c7e966
pkellina <pkellina1@gmail.com>
2022-09-15 16:14:26 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -10,6 +9,7 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
