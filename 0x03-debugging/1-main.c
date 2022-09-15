1858e138e8c0fd3e03266b9c1c6af59894b57dc8
pkellina <pkellina1@gmail.com>
2022-09-15 15:38:32 -0700

    Task 0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	/**
-	 * i = 0
-	 *
+	i = 0
+	/*
 	 * while (i < 0)
-	 * {
-	 * 	putchar(i);
-	 * }
+	 {
+	 * Infinite loop - No increase of variable
+	 * putchar(i);
+	 }
 	 */
 	printsf("Infinite loop avoided! \\o/\n");
 	return (0);
