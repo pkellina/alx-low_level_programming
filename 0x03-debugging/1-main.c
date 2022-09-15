51149343a0cb6704e80b305a26fd8722971f63b4
pkellina <pkellina1@gmail.com>
2022-09-15 13:38:20 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	/**
+	 * i = 0
+	 *
+	 * while (i < 0)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	printsf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
