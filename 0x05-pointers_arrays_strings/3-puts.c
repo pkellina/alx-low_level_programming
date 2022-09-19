f71afe9b8d8b1e8d9e80c5347ac822af587ed37c
pkellina <pkellina1@gmail.com>
2022-09-19 15:46:40 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,7 +4,6 @@
 /**
  * _puts - prints a string
  * @str: string to print
- * 
  * Description: prints a string
  * On success: returns no error
  */
@@ -13,10 +12,10 @@ void _puts(char *str)
 {
 		int i = 0;
 
-			while (*(str + i) != '\0')
-			{
-				putchar(*(str + i));
-				i++;
-			}
-			putchar(10);
+		while (*(str + i) != '\0')
+		{
+			putchar(*(str + i));
+			i++;
+		}
+		putchar(10);
 }
