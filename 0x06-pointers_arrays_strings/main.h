30c9317488c159fc51b8deacc6d3df9661798c32
pkellina <pkellina1@gmail.com>
2022-09-21 07:35:15 -0700

    Task 6

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _main_H_
+#define main_H_
 
 /**
  * void prototypes()
@@ -19,4 +19,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-#endif /*MAIN_H*/
+#endif 
