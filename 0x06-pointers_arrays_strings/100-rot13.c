1c76e38a6eeec893572776dbc626a111746e8686
pkellina <pkellina1@gmail.com>
2022-09-21 09:37:43 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,8 +2,8 @@
 
 /**
  * rot13 - encodes a string using rot13
- * @s: input string.
- * Return: the pointer to dest.
+ * @s: input string
+ * Return: the pointer to dest
  */
 
 char *rot13(char *s)
