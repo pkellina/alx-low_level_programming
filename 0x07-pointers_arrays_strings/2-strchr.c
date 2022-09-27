5fdcdfe720c0318e5895002f92b04400ec778df2
pkellina <pkellina1@gmail.com>
2022-09-26 20:27:07 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- * _strchr - prints found c
- * @s: pointer to char
- * @c: char params to found
- * Return
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to check for
+ *
+ * Return: pointer to spot in s with c or null
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; *(s + 1): i++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
+		if (*(s + 1) == c)
+			return (s + i);
 	}
-	if (*s == c)
-	{
-		return (s);
-	}
+	if (*(s + i) == c)
+		return (s + i);
 	return (0);
 }
