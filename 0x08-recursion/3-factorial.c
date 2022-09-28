d3712c23ed639b88144bef7c3d693eec3498cde4
pkellina <pkellina1@gmail.com>
2022-09-28 11:00:06 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of n
+ * @n: integer
+ *
+ * Return: on success 1
+ * on error, -1 is returnedwith apprpraite errno
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
