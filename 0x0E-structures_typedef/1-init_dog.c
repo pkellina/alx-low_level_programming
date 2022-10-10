1ee8e1f5a69343f218d04bd98e21e137752f3d4c
pkellina <pkellina1@gmail.com>
2022-10-10 15:05:38 -0700

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,7 +5,7 @@
  * @d: Variable to initialize.
  * @name: Dog's name.
  * @age: Dog's age.
- * owner: Dog's owner.
+ * @owner: Dog's owner.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
