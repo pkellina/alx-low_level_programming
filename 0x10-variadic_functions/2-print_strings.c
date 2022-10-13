18c6a116082b74fb836afdf45e5429d10eac1f83
pkellina <pkellina1@gmail.com>
2022-10-13 16:45:04 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 
 		if (index != (n - 1) && separator != NULL)
-														printf("%s", separator);
+			printf("%s", separator);
 	}
 
 	printf("\n");
