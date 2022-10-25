e8142b543ad361f40185fd836b95111b7ac62c3e
pkellina <pkellina1@gmail.com>
2022-10-25 05:20:55 -0700

    Task 9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *  * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
  *  @head: pointer to the first node in the list
  *  @index: index of the node to delete
  *  Return: 1 (Success), or -1 (Fail)
